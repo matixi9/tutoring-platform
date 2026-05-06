8cc9ac2ba76bf41235861e822342909d59364cb1
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-05-06 23:00:49 +0200

    feat: add Navbar component

---
+++ frontend/TutoringPlatformFeReact/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="logo">
+                <span className="logo-k">k</span>
+                <span className="logo-text">redkorepetycje</span>
+            </Link>
+
+            {/* <div className="nav-center">
+                 <Link to="/how-it-works" className="nav-link">Jak to działa?</Link>
+            </div> */}
+
+            <div className="nav-right">
+                <button className="btn-secondary">Logowanie</button>
+                <button className="btn-primary">Dołącz jako tutor</button>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
