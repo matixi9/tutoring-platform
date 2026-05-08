e570b1628c0d6272eaf20e6726f59701ea22fc61
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-05-08 15:15:58 +0200

    fix: change AuthGuard in router

--- frontend/TutoringPlatformFeReact/src/routes/router.tsx
+++ frontend/TutoringPlatformFeReact/src/routes/router.tsx
@@ -1,22 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Home from "../pages/Home";
 import Wrapper from '../components/Wrapper';
-
+import Home from '../pages/Home';
+import AuthGuard from './AuthGuard';
 
 
 export const router = createBrowserRouter([
     {
-    path: "/",
-    element: <Wrapper />,
-    children: [
-      {
-        path: "/", 
-        element: <Home />,
-      },
-    //   {
-    //     path: "/login", 
-    //     element: <Login />, 
-    //   },
-    ],
-  },
+        path: "/",
+        element : <Wrapper />,
+        children: [
+            {path: "/", element: <Home/>},
+            // {path: "/login", element: <Login/>},
+
+            {
+                element: <AuthGuard/>,
+                children: [
+                    // {path : "/moje-lekcje", element: <MyLessons />},
+                    // { path: "/dodaj-ogloszenie", element: <CreateOffer /> },
+                    // { path: "/profil", element: <Profile /> },
+                ]
+
+            }
+
+        ],
+    },
 ]);
