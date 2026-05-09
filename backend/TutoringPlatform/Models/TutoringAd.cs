55a84c164f34344d199cd464ca6d3a5b3fefbec7
matixi9 <290216@student.pwr.edu.pl>
2026-05-09 19:07:49 +0200

    feat: create TutoringAd entity

---
+++ backend/TutoringPlatform/Models/TutoringAd.cs
@@ -0,0 +1,14 @@
+﻿namespace TutoringPlatform.Models;
+
+public class TutoringAd
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required int Price { get; set; }
+    public required bool isOnline { get; set; }
+    public required bool isAvailable { get; set; }
+    
+    public required int Tutorid { get; set; }
+    public User? Tutor { get; set; }
+}
