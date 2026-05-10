071ceaeaaf4cbad43a6c0ae104a70ca19e4983c5
matixi9 <290216@student.pwr.edu.pl>
2026-05-10 17:35:57 +0200

    feat: add TutoringAdDto

---
+++ backend/TutoringPlatform/DTOs/TutoringAdDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TutoringPlatform.DTOs;
+
+public class TutoringAdDto
+{
+    public int Id { get; set; }
+    public required string Subject { get; set; }
+    public decimal Price { get; set; }
+    public bool IsOnline { get; set; }
+    public required string TutorName { get; set; }
+    public int TutorId { get; set; }
+}
