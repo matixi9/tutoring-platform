59af535e1f8896b9a6beac6a8c96f04cfdef8089
matixi9 <290216@student.pwr.edu.pl>
2026-05-10 17:52:28 +0200

    feat: add validators for TutoringAd DTOs

--- backend/TutoringPlatform/DTOs/UpdateTutoringAdDto.cs
+++ backend/TutoringPlatform/DTOs/UpdateTutoringAdDto.cs
@@ -5,6 +5,6 @@ public class UpdateTutoringAdDto
     public required string Title { get; set; }
     public required string Description { get; set; }
     public required int Price { get; set; }
-    public required bool isOnline { get; set; }
-    public required bool isAvailable { get; set; }
+    public required bool IsOnline { get; set; }
+    public required bool IsAvailable { get; set; }
 }
