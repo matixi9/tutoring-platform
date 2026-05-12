1f646065139e661bcd7f8a266a8795b08aff2ab5
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-05-12 21:53:23 +0200

    refactor: move login and register components to /pages, change their routes

--- frontend/TutoringPlatformFeReact/src/routes/router.tsx
+++ frontend/TutoringPlatformFeReact/src/routes/router.tsx
@@ -2,8 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import Wrapper from '../components/Wrapper';
 import Home from '../pages/Home';
 import AuthGuard from './AuthGuard';
-import Login from '../components/Login';
-import Register from '../components/Register';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 
 export const router = createBrowserRouter([
