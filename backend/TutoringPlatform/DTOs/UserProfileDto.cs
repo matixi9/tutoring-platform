b59b6b870e86868b1b1f87cd58f3046ec5708709
matixi9 <290216@student.pwr.edu.pl>
2026-05-14 00:46:06 +0200

    feat: add UserProfileDto

---
+++ backend/TutoringPlatform/DTOs/UserProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TutoringPlatform.DTOs;
+
+public class UserProfileDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Role { get; set; }
+}
